8562b66c07511aa42bb08e07d233faaf73bd2d0b
Pavel Drovnin <pavel98rus@gmail.com>
2023-11-15 21:27:24 +0300

    model time validator fix

--- shop_bank/shops/validators.py
+++ shop_bank/shops/validators.py
@@ -1,10 +1,10 @@
-import datetime
+from datetime import datetime
 
 from django.core.exceptions import ValidationError
 
 
 def validate_time(value):
-    min_value = datetime.strptime('00:00', '%H:%M')
-    max_value = datetime.strptime('23:59', '%H:%M')
+    min_value = datetime.strptime('00:00', '%H:%M').time()
+    max_value = datetime.strptime('23:59', '%H:%M').time()
     if not min_value <= value <= max_value:
         raise ValidationError('Проверьте вводимое время')
