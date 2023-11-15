3c501350e59f0fbd16b1598be3942f8443d0c320
Pavel Drovnin <pavel98rus@gmail.com>
2023-11-15 21:26:39 +0300

    model fix

--- shop_bank/shops/models.py
+++ shop_bank/shops/models.py
@@ -5,27 +5,27 @@ from .validators import validate_time
 
 
 class City(models.Model):
-    city_name = models.CharField(verbose_name='Название города',
-                                 max_length=30)
-    
+    name = models.CharField(verbose_name='Название города',
+                            max_length=30)
+
     def __str__(self):
         return f'{self.name}'
 
 
 class Street(models.Model):
-    street_name = models.CharField(verbose_name='Название улицы',
-                                   max_length=50)
+    name = models.CharField(verbose_name='Название улицы',
+                            max_length=50)
     city = models.ForeignKey(City, on_delete=models.CASCADE,
                              related_name='streets',
                              verbose_name='Город')
-    
+
     def __str__(self):
         return f'{self.name}'
 
 
 class Shop(models.Model):
-    shop_name = models.CharField(verbose_name='Название магазина',
-                                 max_length=30)
+    name = models.CharField(verbose_name='Название магазина',
+                            max_length=30)
     street = models.ForeignKey(Street, on_delete=models.CASCADE,
                                related_name='shops',
                                verbose_name='Улица')
@@ -37,6 +37,9 @@ class Shop(models.Model):
                                  validators=(validate_time,))
     time_close = models.TimeField(verbose_name='Время закрытия',
                                   validators=(validate_time,))
-    
+
+    def get_city(self):
+        return self.street.city
+
     def __str__(self):
         return f'{self.name}'
