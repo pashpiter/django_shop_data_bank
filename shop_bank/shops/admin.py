a01401cfeb55e65daf8fbf1073e2c399253d46a4
Pavel Drovnin <pavel98rus@gmail.com>
2023-11-15 21:26:23 +0300

    add admin panel

--- shop_bank/shops/admin.py
+++ shop_bank/shops/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import City, Street, Shop
+
+
+class AdminCity(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+
+
+class AdminStreet(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'city')
+    list_filter = ('city',)
+
+
+class AdminShop(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'get_city', 'street', 'house_number',
+                    'time_open', 'time_close')
+
+    def get_city(self, obj):
+        return obj
+    get_city.short_description = 'Город'
+
+
+admin.site.register(City, AdminCity)
+admin.site.register(Street, AdminStreet)
+admin.site.register(Shop, AdminShop)
