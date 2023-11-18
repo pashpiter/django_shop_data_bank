fdd9c2cd61b4da9672ba2aca37483ae475149bf6
Pavel Drovnin <pavel98rus@gmail.com>
2023-11-18 18:38:12 +0300

    exception_handler fix

--- shop_bank/shop_bank/utils.py
+++ shop_bank/shop_bank/utils.py
@@ -4,6 +4,6 @@ from rest_framework.views import exception_handler
 def custom_exception_handler(exc, context):
     """При любой ошибке возварщает код 400"""
     response = exception_handler(exc, context)
-    if response.status_code:
+    if response and response.status_code:
         response.status_code = 400
     return response
